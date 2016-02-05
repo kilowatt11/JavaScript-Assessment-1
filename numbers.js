0dff2c53e163b044acf3de85c39c35d47da3a985
kilowatt11 <kilowatt11@cableone.net>
2016-02-04 22:49:11 -0700

    solutions for numbers assessment.

--- numbers.js
+++ numbers.js
@@ -2,10 +2,21 @@
 var num10 = 10;
 var string8 = '8';
 var one = 1;
-//2. write a loop that will log only numbers divisible by 3 between 20 - 100
+console.log(num10 + parseInt(string8) + one);
 
+
+
+//2. write a loop that will log only numbers divisible by 3 between 20 - 100
+for (var index =20; index < 100; index++) {
+    if(index % 3==0)
+    console.log(index);
+    
+}
 
 
 //3. find the class average given the following test scores
 
 var scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98]; 
+var total = 0;
+for(var i in scores) { total += scores[i];}
+console.log(total / scores.length);
